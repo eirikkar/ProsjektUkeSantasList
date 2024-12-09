f5803f61248be3aadf4a7467642f7d1fcdd696af
LHT082024 <LHT082024@gmail.com>
2024-12-09 17:22:15 +0100

    have to give up for now

--- SortingStuff.cs
+++ SortingStuff.cs
@@ -10,6 +10,12 @@ namespace ProsjektUkeSantasList
     public class SortingStuff
     {
         Person person = new Person();
+        GoodAndBadList goodAndBadList = new()
+        {
+            GoodList = new List<string> { "good kids" },
+            BadList = new List<string> { "bad kids" }
+
+        };
         //adding points to people.
         public void AddingPoints()
         {
@@ -19,10 +25,10 @@ namespace ProsjektUkeSantasList
 
             //converts string to object
             List<Person> people = JsonSerializer.Deserialize<List<Person>>(data);
+            int peoplePoints = 0;
 
             foreach (var person in people)
             {
-                int peoplePoints = 0;
 
                 // toilet paper
                 if (person.ToiletPaperOutward == true)
@@ -87,10 +93,21 @@ namespace ProsjektUkeSantasList
                 //     peoplePoints -= 30;
                 // }
 
-                person.Points = peoplePoints;
-                Console.WriteLine($"{person.Name}: {person.Points}");
-            }
+                // person.Points = peoplePoints;
+                // Console.WriteLine($"{person.Name}: {person.Points}");
 
+                if (peoplePoints >= 10)
+                {
+                    goodAndBadList.BadList.Add(person.Name);
+                }
+                else
+                {
+                    goodAndBadList.GoodList.Add(person.Name);
+                }
+
+                Console.WriteLine("Good List: " + string.Join(", ", goodAndBadList.GoodList));
+                Console.WriteLine("\n\nBad List: " + string.Join(", ", goodAndBadList.BadList));
+            }
 
         }
     }
