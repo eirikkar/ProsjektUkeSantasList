1dbc55ca99d8d8cc94de53147a898650292790a5
eirikkar <eirikkar@proton.me>
2024-12-11 10:44:31 +0100

    Add ElfList class to Models directory with predefined elf entries

---
+++ Models/ElfList.cs
@@ -0,0 +1,41 @@
+public class ElfList
+{
+    public List<Alvene> elfList = new List<Alvene>
+    {
+        new Alvene
+        {
+            Name = "Askasleikir",
+            Spesialisering = "elektronikk",
+            Ansvar = "kjøpe online",
+            Gift = "Flashbackinator",
+        },
+        new Alvene
+        {
+            Name = "Pottasleikir",
+            Spesialisering = "keramikk",
+            Ansvar = "at gavenne ser bra ut med sloife",
+            Gift = "Aincent greek pot",
+        },
+        new Alvene
+        {
+            Name = "Bankerinie",
+            Spesialisering = "søm",
+            Ansvar = "at skjærebrett julegaver",
+            Gift = "dragon leather robe",
+        },
+        new Alvene
+        {
+            Name = "Sledda",
+            Spesialisering = "trearbeid",
+            Ansvar = "at sleden virker bra",
+            Gift = "vikingskip",
+        },
+        new Alvene
+        {
+            Name = "Smieknusser",
+            Spesialisering = "smie",
+            Ansvar = "for reinsdyrene",
+            Gift = "neckalce that tells you the weather",
+        },
+    };
+}
