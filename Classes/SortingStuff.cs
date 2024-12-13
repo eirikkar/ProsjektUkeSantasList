a169fe6f7c0936cb3190a477d9c6ee3a9dc167ca
LHT082024 <LHT082024@gmail.com>
2024-12-13 11:37:43 +0100

    sorted shit better

--- Classes/SortingStuff.cs
+++ Classes/SortingStuff.cs
@@ -1,8 +1,14 @@
+using ProsjektUkeSantasList.Ifs;
+
 namespace ProsjektUkeSantasList
 {
     public class SortingStuff
     {
-        //impliments other classes needed to make the logic work
+        //calling the different classes from the ifs folder
+        TrueFalse trueFalse = new();
+        MusicGenres musicGenres = new();
+        HomeAdress homeAdress = new();
+        Cars cars = new();
 
         //adding points to people.
         public List<Person> AddingPoints(List<Person> people)
@@ -11,95 +17,24 @@ namespace ProsjektUkeSantasList
             //and assigns them points based on their variables
             foreach (var person in people)
             {
-                int peoplePoints = 0;
 
                 // toilet paper
-                if (person.ToiletPaperOutward == true)
-                {
-                    peoplePoints += 10;
-                }
-                else
-                {
-                    peoplePoints -= 10;
-                }
+                trueFalse.ToiletPaper(person);
 
                 //Donating to charity
-                if (person.DonatesToCharity == true)
-                {
-                    peoplePoints += 50;
-                }
-                else
-                {
-                    peoplePoints -= 50;
-                }
+                trueFalse.Donating(person);
 
                 //washes hands
-                if (person.WashesHands == true)
-                {
-                    peoplePoints += 20;
-                }
-                else
-                {
-                    peoplePoints -= 20;
-                }
+                trueFalse.Washing(person);
 
                 //music genres
-                if (person.MusicGenres == null)
-                {
-                    throw new Exception("Error music genre is null");
-                }
-                if (
-                    person.MusicGenres.Contains("J-pop")
-                    || person.MusicGenres.Contains("Folk")
-                    || person.MusicGenres.Contains("Blues")
-                )
-                {
-                    peoplePoints += 50;
-                }
-                else
-                {
-                    peoplePoints -= 50;
-                }
+                musicGenres.judgingMusic(person);
 
                 //homeadress
-                if (person.HomeAdress == null)
-                {
-                    throw new Exception("Error Home adress is null");
-                }
-                if (
-                    person.HomeAdress.Contains("A")
-                    || person.HomeAdress.Contains("74")
-                    || person.HomeAdress.Contains("B")
-                )
-                {
-                    peoplePoints += 15;
-                }
-                else
-                {
-                    peoplePoints -= 15;
-                }
+                homeAdress.StalkingUwU(person);
 
                 //car model
-                switch (person.CarModel)
-                {
-                    case "Toyota":
-                        peoplePoints += 25;
-                        break;
-
-                    //sosse bil
-                    case "BMW":
-                        peoplePoints -= -50;
-                        break;
-
-                    case "Tesla":
-                        peoplePoints -= 50;
-                        break;
-
-                    default:
-                        peoplePoints += 20;
-                        break;
-                }
-                person.Points = peoplePoints;
+                cars.shittyOrCoolCar(person);
             }
             return people;
         }
---
+++ Ifs/HomeAdress.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProsjektUkeSantasList.Ifs
+{
+    public class HomeAdress
+    {
+        public void StalkingUwU(Person person)
+        {
+            if (person.HomeAdress == null)
+            {
+                throw new Exception("Error Home adress is null");
+            }
+            if (
+                person.HomeAdress.Contains("A")
+                || person.HomeAdress.Contains("74")
+                || person.HomeAdress.Contains("B")
+            )
+            {
+                person.Points += 15;
+            }
+            else
+            {
+                person.Points -= 15;
+            }
+
+        }
+    }
+}
